adbe9b6e4ed20556eac0e7c11ef2bcb40a21a84d
Open Alarm Management System <openams@oams>
2016-03-19 17:24:40 -0500

    Agrega gitignore

--- oams_php_script_private/_pg_conn_string.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-ini_set('display_errors', 1);
-//$conn_string = "host=localhost port=5432 dbname=farmaenlace user=postgres password=pg4321";
-$conn_string = "host=localhost port=5432 dbname=open_ams user=postgres password=pg1234";
-//$pGdbconn = pg_connect($conn_string)  or die("Could not connect");
-?>
