e8e78c0ec212eebb49d16e19780c0798e587a761
Open Alarm Management System <openams@oams>
2016-04-04 13:47:35 -0500

    Corregido bug que no permite cargar los abonados en el mapa.

--- map.php
+++ map.php
@@ -30,7 +30,7 @@
 <?php 
 require_once "lib/custom/uDC/pguDC.php";
     $db = new pguDC();
-    $db->connect();
+    $db->connect("accounts");
 
 $result = "[{}]";
 
@@ -39,18 +39,22 @@ $result = "[{}]";
 switch($_GET["maptype"]){
 case 0:
 // Muestra todos los abonados del sistema
-$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE enabled = true AND geox != 0 AND geoy != 0;", array()));
+//$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE enabled = true AND geox != 0 AND geoy != 0;", array()));
+$result = $db->query_params_result_as_json("SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE enabled = true AND geox != 0 AND geoy != 0", array());
 break;
 case 1:
 // Muestra solo el abonado seleccionado
 if(isset($_GET["idcontact"]) && $_GET["idcontact"] > 0){
-$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact = $1::bigint AND geox != 0 AND geoy != 0;", array($_GET["idcontact"])));
+//$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact = $1::bigint AND geox != 0 AND geoy != 0;", array($_GET["idcontact"])));
+$result = $db->query_params_result_as_json("SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact = $1::bigint AND geox != 0 AND geoy != 0", array($_GET["idcontact"]));
 }
 break;
 case 2:
 // Muestra los abonados asignados al contacto pasado como parametro
 if(isset($_GET["idcontact"]) && $_GET["idcontact"] > 0){
-$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact IN (SELECT idaccount FROM view_account_contacts WHERE idcontact = $1::bigint AND appointment = 'Técnico Responsable') AND geox != 0 AND geoy != 0;", array($_GET["idcontact"])));
+
+$result = $db->query_params_result_as_json("SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact IN (SELECT idaccount FROM view_account_contacts WHERE idcontact = $1::bigint AND appointment = 'Técnico Responsable') AND geox != 0 AND geoy != 0;", array($_GET["idcontact"]));
+//$result =  pguDC::result_to_json("accounts", pg_query_params($db->connection, "SELECT account, last_name, first_name, geox, geoy, address, address_ref FROM accounts WHERE idcontact IN (SELECT idaccount FROM view_account_contacts WHERE idcontact = $1::bigint AND appointment = 'Técnico Responsable') AND geox != 0 AND geoy != 0;", array($_GET["idcontact"])));
 }
 break;
 }
