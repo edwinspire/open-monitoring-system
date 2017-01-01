1e56e5ff69550fdfba6b2a5843a262be4dd8869e
Edwin De La Cruz <edwinspire@edwinspire>
2017-01-01 10:18:19 -0500

    Modificado app_monitor_general para referir a un path mas generico

--- src/app_client/js/dojo/custom/app_monitor_general/app_monitor_general.js
+++ src/app_client/js/dojo/custom/app_monitor_general/app_monitor_general.js
@@ -1,7 +1,7 @@
 define(['dojo/_base/declare',
     'dijit/_Widget',
     'dijit/_Templated',
-    'dojo/text!app_monitor_general/app_monitor_general.html',
+    'dojo/text!Widget/app_monitor_general/app_monitor_general.html',
     "dojo/dom-class",
     "dojo/on"
 ], function (declare, _Widget, _Templated, templateString, domClass, on) {
