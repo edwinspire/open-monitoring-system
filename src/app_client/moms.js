7da97b676a770c5ced4037c8622feff09feee340
Edwin De La Cruz <edwinspire@debianlap.lan>
2017-01-07 03:53:41 -0500

    Se crea un evento que dispara el menu cuando se hace click en un item que realizara una accion

---
+++ src/app_client/moms.js
@@ -0,0 +1,174 @@
+require(["dojo/ready", 
+  "dojo/cookie",
+  "Widget/EventServerIO/EventServerIO",
+  "Widget/UserNotifications/UserNotifications",
+  "dojo/_base/connect",
+  "dojo/_base/window", 
+  "dojo/on",
+  "dojo/topic",
+  "dojo/dom-class",
+  "dojo/dom-attr",
+  "dojo/query",
+  "dojo/mouse",
+  "dojo/NodeList-dom",
+  "dojo/dom-construct", 
+  "Widget/app_monitor_general/app_monitor_general", 
+  "Widget/app_lista_precios_promo_eco/app_lista_precios_promo_eco", 
+  "Widget/app_lista_precios_promo_medi/app_lista_precios_promo_medi", 
+  "Widget/app_lista_precios_pnatural/app_lista_precios_pnatural", 
+  "Widget/app_lista_precios_eco/app_lista_precios_eco", 
+  "Widget/app_lista_precios_medi/app_lista_precios_medi", 
+  "app_monitor_network_devices/app_monitor_network_devices", 
+  "Geolocations/Geolocations", 
+  "app_accounts_table/app_accounts_table", 
+  "app_account/app_account",
+  "app_tables_views_config/app_tables_views_config",
+  "app_login_report/app_login_report"
+  ], function(ready, 
+    cookie,
+    EventServerIO,
+    UserNotifications,
+    connect, 
+    win,
+    on,
+    topic,
+    domClass,
+    domAttr,
+    query, 
+    mouse,
+    nodeListDom,
+    domConstruct, 
+    app_monitor_general, 
+    app_lista_precios_promo_eco, 
+    app_lista_precios_promo_medi, 
+    app_lista_precios_pnatural, 
+    app_lista_precios_eco, 
+    app_lista_precios_medi, 
+    app_monitor_network_devices, 
+    app_geolocations, 
+    app_accounts_table, 
+    app_account,
+    app_tables_views_config,
+    app_login_report
+    ){
+   ready(function(){
+
+
+var Mainmenu = dojo.byId("main_menu");
+on(Mainmenu, "clickitem", function(e){
+console.debug(e);
+}); 
+
+/*
+ var ESIO = new EventServerIO();
+var NOTIF = new UserNotifications();
+
+
+    var FullName = dojo.byId('FullName');
+//     var SIDEBAR_MENU = dojo.byId('sidebar-menu');
+var bcloseitems = dojo.byId('support_empty_button_to_close_the_remaining_items_when_loading_an_app');
+var PageContent = dojo.byId("PageContent");
+var GeneralEmpresaAbonados = dojo.byId("GeneralEmpresaAbonados");
+var MonitorGeneral = dojo.byId("MonitorGeneral");
+var BODY = win.body();
+
+var widget  = {};
+
+FullName.innerHTML = cookie('oms_fullname');
+//on(node, mouse.enter, hoverHandler);
+
+on(PageContent, mouse.enter, function(){
+
+//console.debug('Estamos dentro');
+if (!domClass.contains(BODY, "nav-md")){  
+ bcloseitems.click();
+}
+
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(GeneralEmpresaAbonados, 'onclick', function(){
+  OpenApp(new app_account());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("admin_tables_views_edit"), 'onclick', function(){
+  OpenApp(new app_tables_views_config());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId('Idapp_login_report'), 'onclick', function(){
+  OpenApp(new app_login_report());
+});
+
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("GeneralEmpresaGeolocalizacion"), 'onclick', function(){
+  OpenApp(new app_geolocations());
+});   
+
+
+//////////////////////////////////////////////////////
+dojo.connect(MonitorGeneral, 'onclick', function(){
+  OpenApp(new app_monitor_general());
+}); 
+
+//////////////////////////////////////////////////////
+dojo.connect(Monitor_NetworkDevices, 'onclick', function(){
+  OpenApp(new app_monitor_network_devices());
+}); 
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("utilitarios_lista_precios_farma_promo_eco"), 'onclick', function(){
+  OpenApp(new app_lista_precios_promo_eco());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("utilitarios_lista_precios_farma_promo_medi"), 'onclick', function(){
+  OpenApp(new app_lista_precios_promo_medi());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("utilitarios_lista_precios_pnatural"), 'onclick', function(){
+  OpenApp(new app_lista_precios_pnatural());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("utilitarios_lista_precios_farma_eco"), 'onclick', function(){
+  OpenApp(new app_lista_precios_eco());
+});
+
+
+///////////////////////////////////////////////////////////
+dojo.connect(dojo.byId("utilitarios_lista_precios_farma_medi"), 'onclick', function(){
+  OpenApp(new app_lista_precios_medi());
+});
+
+*/
+
+
+function OpenApp(_widget){
+
+
+  if(widget.app){
+    widget.app.destroy();
+  }
+
+//console.log(registry.findWidgets(PageContent));
+//console.log(widget);
+domConstruct.empty(PageContent);
+widget.app = _widget;
+domConstruct.place(_widget.domNode, PageContent, 'only');
+}
+
+
+});
+ });
