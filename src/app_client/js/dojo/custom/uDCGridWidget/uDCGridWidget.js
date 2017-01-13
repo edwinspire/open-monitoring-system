9d38c7b866e0cc44672f8e540994edf46bab0ca3
Edwin De La Cruz <edwinspire@debianlap.lan>
2017-01-12 20:18:06 -0500

    Funciona el menu para refresco de la tabla

--- src/app_client/js/dojo/custom/uDCGridWidget/uDCGridWidget.js
+++ src/app_client/js/dojo/custom/uDCGridWidget/uDCGridWidget.js
@@ -1,74 +1,77 @@
 define(['dojo/_base/declare',
-  'dijit/_Widget',
-  'dijit/_Templated',
-  'dojo/text!Widget/uDCGridWidget/uDCGridWidget.html',
-  "uDCGrid/uDCGrid",
-  "dojo/dom-construct",
-  "dojo/on",
-  "dojo/dom-style",
-  "dojo/window",
-  "dojo/topic",
-  'dstore/Memory',
-  'dstore/Trackable',
-  "dijit/form/TextBox",
-  "uDC/uDC",
-  "dijit/ToolbarSeparator",
-  "dijit/form/NumberSpinner",
-  "dijit/ConfirmTooltipDialog",
-  "dijit/RadioMenuItem",
-  "dijit/MenuItem",
-  "dijit/PopupMenuItem",
-  "dijit/Menu"
-  ],function(declare,_Widget,_Templated,templateString, uDCGrid, domConstruct, on, domStyle,  w){
+	'dijit/_Widget',
+	'dijit/_Templated',
+	'dojo/text!Widget/uDCGridWidget/uDCGridWidget.html',
+	"uDCGrid/uDCGrid",
+	"dojo/dom-construct",
+	"dojo/on",
+	"dojo/dom-style",
+	"dojo/window",
+	"dojo/topic",
+	'dstore/Memory',
+	'dstore/Trackable',
+	"dijit/form/TextBox",
+	"uDC/uDC",
+	"dijit/ToolbarSeparator",
+	"dijit/form/NumberSpinner",
+	"dijit/ConfirmTooltipDialog",
+	"dijit/RadioMenuItem",
+	"dijit/MenuItem",
+	"dijit/PopupMenuItem",
+	"dijit/Menu"
+	],function(declare,_Widget,_Templated,templateString, uDCGrid, domConstruct, on, domStyle,  w){
 
-   return declare([ _Widget, _Templated], {
-     widgetsInTemplate:true,
-     templateString:templateString,
-     Config: {},
-     Grid: null,
-     _GridFieldsToFilter: [],
-     _enable_load: false,
-     refreshMode: 0,
+		return declare([ _Widget, _Templated], {
+			widgetsInTemplate:true,
+			templateString:templateString,
+			Config: {},
+			Grid: null,
+			_enable_load: false,
+			refreshMode: 0,
 
-     _GStructure: {},
-     _IntervalRefresh: null,
+			_GStructure: {},
+			_IntervalRefresh: null,
+			_RemainDisabledRefresh: 0,
+			_last_select: null,
+			initialQuery: false,
+			_setTitlegridAttr: { node: "TitleGrid", type: "innerHTML" },
+			postCreate: function () {
+				var t = this;
 
-     _last_select: null,
-     initialQuery: false,
-     _setTitlegridAttr: { node: "TitleGrid", type: "innerHTML" },
-     postCreate: function () {
-      var t = this;
-
-      t.Search.on('Change', function (e) {
-        console.debug(e);
-            t.Grid.Filter(e.value);
-          });
-
-
-          t._IntervalRefresh = setInterval(function () {
-            if (t.Grid.refreshMode == 3) {
-t.LabelRefreshTimer.innerHTML = '213213213';
-console.debug('Tempo');
-
-            }
-          }, 1000);
+				t.Search.on('Change', function (e) {
+					console.debug(e);
+					t.Grid.Filter(e.value);
+				});
 
 
-          on(t.RefreshAuto, 'click', function (e) {
-            t.Grid.refreshMode = 1;
-            t.set('labeltimerrefresh', '120');
-           // domClass.replace(t.DisabledRefreshTempo, "grid_icon_refresh_white", "grid_icon_refresh_green grid_icon_refresh_red grid_icon_refresh_blue");
-           // domClass.replace(t.Refresh, "grid_icon_refresh_white", "grid_icon_refresh_green grid_icon_refresh_red grid_icon_refresh_blue");
-            console.log('Se activa el refresco ');
-            t.Grid.Select(t.Grid._last_select);
-             
-              });
+				t._IntervalRefresh = setInterval(function () {
+					if (t.Grid.refreshMode == 3 && t._RemainDisabledRefresh > 0) {
+						t._RemainDisabledRefresh--;
+						t.RefreshTempo.set('label', 'Desactivado por '+t._RemainDisabledRefresh+' seg.');
+						console.debug('Tempo', t.LabelRefreshTimer);
 
-          on(t.RefreshTempo, 'click', function (e) {
-            t.Grid.refreshMode = 3;
-            t.LabelRefreshTimer.innerHTML = 120;
-            });
+					}else if(t._RemainDisabledRefresh < 1){
+						t.RefreshTempo.set('label', 'Desactivar');
+						t.Grid.refreshMode = 1;
+						t._RemainDisabledRefresh = 120;	
+					}else{
+						t.RefreshTempo.set('label', 'Desactivar');
+					}
+				}, 1000);
 
+
+				on(t.RefreshAuto, 'click', function (e) {
+					t.Grid.refreshMode = 1;
+					console.log('Se activa el refresco ');
+					t.Grid.Select(t.Grid._last_select);
+				});
+
+				on(t.RefreshTempo, 'click', function (e) {
+					t.Grid.refreshMode = 3;
+					t.RefreshTempo.set('label', 'Desactivar');
+					console.debug(t.LabelRefreshTimer);
+					t._RemainDisabledRefresh = 120;
+				});
 
 /*
 
@@ -121,141 +124,138 @@ console.debug('Tempo');
           });
 
           t.CollapseLink.set('contentelement', t.XContent);
-*/
+          */
 
           t._create_grid();
 
 
-        },
-        uninitialize: function(){
-          var t = this;
-          console.log('Llama para matar el widget');
-          clearInterval(t._IntervalRefresh);
+      },
+      uninitialize: function(){
+      	var t = this;
+      	console.log('Llama para matar el widget');
+      	clearInterval(t._IntervalRefresh);
 
 
-          t.Grid.uninitialize();
+      	t.Grid.uninitialize();
 
-          t.Grid.destroy();
-          delete t.Grid;
-          t.Grid = null;
+      	t.Grid.destroy();
+      	delete t.Grid;
+      	t.Grid = null;
 
-        },     
-        _setFormnewrowAttr: function (_node, _udc_class, _title) {
-          var t = this;
-          console.log(_node);
-          if (_title) {
-            t.DialogNew.set('title', _title);
-          }
-          t.uDC.set('config', { SelectorClass: _udc_class, NodeContainer: t.DialogNew.domNode });
-          domConstruct.place(_node, t.DialogNewContent, "replace");
-          return this;
-        },
-        _create_grid: function () {
+      },     
+      _setFormnewrowAttr: function (_node, _udc_class, _title) {
+      	var t = this;
+      	console.log(_node);
+      	if (_title) {
+      		t.DialogNew.set('title', _title);
+      	}
+      	t.uDC.set('config', { SelectorClass: _udc_class, NodeContainer: t.DialogNew.domNode });
+      	domConstruct.place(_node, t.DialogNewContent, "replace");
+      	return this;
+      },
+      _create_grid: function () {
 
-          var t = this;
+      	var t = this;
 
-          domConstruct.empty(t.Contenedor);
-
-
-          t.Grid = new uDCGrid({
-           target: t.target,
-           selectionMode: "none",
-           refreshOnTableChanged: t.refreshOnTableChanged,
-           initialQuery: t.initialQuery,
-           uDCColumns: t.uDCColumns,
-           table: t.table,
-           rowFingerPrint: t.rowFingerPrint,
-           idProperty: t.idProperty
-
-         }, t.Contenedor);
+      	domConstruct.empty(t.Contenedor);
 
 
-          t.Grid.startup();
+      	t.Grid = new uDCGrid({
+      		target: t.target,
+      		selectionMode: "none",
+      		refreshOnTableChanged: t.refreshOnTableChanged,
+      		initialQuery: t.initialQuery,
+      		uDCColumns: t.uDCColumns,
+      		table: t.table,
+      		rowFingerPrint: t.rowFingerPrint,
+      		idProperty: t.idProperty
+
+      	}, t.Contenedor);
 
 
-          on(t.Grid, 'dgrid-set-properties', function (event) {
-
-            t.set('titlegrid', event.properties.title_dgrid);
-
-            setTimeout(function(){
-              t.resize();
-            }, 2000);
-          });
+      	t.Grid.startup();
 
 
-          t.Grid.on('dgrid-error', function (event) {
+      	on(t.Grid, 'dgrid-set-properties', function (event) {
 
-            console.error(event);
-          });
+      		t.set('titlegrid', event.properties.title_dgrid);
 
-          t.Grid.on('dgrid-refresh-complete', function (event) {
-
-            domStyle.set(t.Contenedor, 'height', w.getBox().h-120+'px');
-
-          });
+      		setTimeout(function(){
+      			t.resize();
+      		}, 2000);
+      	});
 
 
+      	t.Grid.on('dgrid-error', function (event) {
 
-          t.Grid.on('dgrid-select', function (event) {
+      		console.error(event);
+      	});
 
-          });
+      	t.Grid.on('dgrid-refresh-complete', function (event) {
 
+      		domStyle.set(t.Contenedor, 'height', w.getBox().h-120+'px');
 
-          t.Grid.on('.dgrid-row:click', function (e) {
-
-           var row = t.Grid.row(e);
-           console.debug('Row clicked:', row);
-           on.emit(t, 'ClickRow', row.data);
-
-         });
-
-
-          if(t.newForm){
-
-           var conf = t.newForm;
-           if(!conf.table || conf.table.length < 1){
-             conf.table = t.table || 'uDCGridWidget.undefined.table.uDC';
-           }
-
-           if(!conf.target || conf.target.length < 1){
-             conf.target = t.target || 'uDCGridWidget.undefined.target.uDC';
-           }
-
-           console.debug(conf);            
-           t.uDC.set('config', conf);
-           domStyle.set(t.Add, 'display', 'block');
-         }else{
-
-         }
-
-
-         return t;
-},
-_notifications: function (_n) {
- topic.publish("/event/user/notify", [_n]);
-},
-resize: function(){
- this.Grid.resize();
-},
-Clear: function(){
-  this.Grid.Clear();
-  return this;
-},
-disabledGrid: function(_disabled){
-  this.Grid.disabled(_disabled);
-},
-_disabled: function(_disable){
-
-  if(_disable){
-    domClass.add(this.domNode, "element-disabled");
-  }else{
-    domClass.remove(this.domNode, "element-disabled");
-  }
-  return this;
-}
+      	});
 
 
 
+      	t.Grid.on('dgrid-select', function (event) {
+
+      	});
+
+
+      	t.Grid.on('.dgrid-row:click', function (e) {
+
+      		var row = t.Grid.row(e);
+      		console.debug('Row clicked:', row);
+      		on.emit(t, 'ClickRow', row.data);
+
+      	});
+
+
+      	if(t.newForm){
+
+      		var conf = t.newForm;
+      		if(!conf.table || conf.table.length < 1){
+      			conf.table = t.table || 'uDCGridWidget.undefined.table.uDC';
+      		}
+
+      		if(!conf.target || conf.target.length < 1){
+      			conf.target = t.target || 'uDCGridWidget.undefined.target.uDC';
+      		}
+
+      		console.debug(conf);            
+      		t.uDC.set('config', conf);
+      		domStyle.set(t.Add, 'display', 'block');
+      	}else{
+
+      	}
+
+
+      	return t;
+      },
+      _notifications: function (_n) {
+      	topic.publish("/event/user/notify", [_n]);
+      },
+      resize: function(){
+      	this.Grid.resize();
+      },
+      Clear: function(){
+      	this.Grid.Clear();
+      	return this;
+      },
+      disabledGrid: function(_disabled){
+      	this.Grid.disabled(_disabled);
+      },
+      _disabled: function(_disable){
+
+      	if(_disable){
+      		domClass.add(this.domNode, "element-disabled");
+      	}else{
+      		domClass.remove(this.domNode, "element-disabled");
+      	}
+      	return this;
+      }
 
 
 
@@ -268,5 +268,8 @@ _disabled: function(_disable){
 
 
 
-});
+
+
+
+  });
 });
