b1d52862f854e22f8eef8c98286dd86d645f105f
Edwin De La Cruz <edwinspire@debianpc>
2017-01-20 16:30:19 -0500

    Nuevo elemento para inserta iconos

---
+++ src/app_client/js/dojo/custom/icon_svg/icon_svg.js
@@ -0,0 +1,17 @@
+define(['dojo/_base/declare',
+    'dijit/_Widget',
+    'dijit/_Templated',
+    'dojo/text!Widget/icon_svg/icon_svg.html'
+], function (declare, _Widget, _Templated, templateString) {
+
+    return declare([_Widget, _Templated], {
+        widgetsInTemplate: true,
+        templateString: templateString,
+
+        postCreate: function () {
+
+
+        }
+
+    });
+});
