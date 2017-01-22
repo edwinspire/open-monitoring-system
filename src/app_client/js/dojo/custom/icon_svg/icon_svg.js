42ead28711ea8d598156a740d74a4911ab88c4f2
Edwin De La Cruz <edwinspire@debianlap.lan>
2017-01-22 18:35:17 -0500

    Se agrega font-awesome en svg como alternativa ya que tenemos error 206 al cargar el ccs original de font-awesome

--- src/app_client/js/dojo/custom/icon_svg/icon_svg.js
+++ src/app_client/js/dojo/custom/icon_svg/icon_svg.js
@@ -2,24 +2,24 @@ define(['dojo/_base/declare',
     'dijit/_Widget',
     'dijit/_Templated',
     'dojo/text!Widget/icon_svg/icon_svg.html'
-], function (declare, _Widget, _Templated, templateString) {
+    ], function (declare, _Widget, _Templated, templateString) {
 
-    return declare([_Widget, _Templated], {
-        widgetsInTemplate: true,
-        templateString: templateString,
-        path_base: 'vendors/foundation-icons/svgs/',
-        postCreate: function () {
-console.log(this.icon);
-if(this.icon){
-this.set("icon", this.icon);	
-}
-
-
-        },
-_setIconAttr: function(icon){
-this.Image.src = this.path_base+icon;
-}
+        return declare([_Widget, _Templated], {
+            widgetsInTemplate: true,
+            templateString: templateString,
+            path_base: 'vendors/fa-svg/svg-black/',
+            postCreate: function () {
+                console.log(this.icon);
+                if(this.icon){
+                    this.set("icon", this.icon);	
+                }
 
 
+            },
+            _setIconAttr: function(icon){
+             this.Image.src = this.path_base+icon;
+         }
+
+
+     });
     });
-});
