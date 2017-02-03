8cdb43520f0b5ed82b6a1c0015923cc57e89fc8b
Edwin De La Cruz <edwinspire@debianpc>
2017-02-03 15:31:05 -0500

    Hay un bug en el almacenamiento de comentarios en app_events_management

--- src/app_client/applib/js/dojo/custom/EventDetails/EventDetails.js
+++ src/app_client/applib/js/dojo/custom/EventDetails/EventDetails.js
@@ -33,27 +33,18 @@
 
                 var t = this;
 
-//        t.CommentField.startup();
+                on(t.DialogCStatusCancel, 'click', function (e) {
+                    t.ContainerChangeStatus.set('open', false);
+                });
 
-on(t.DialogCStatusCancel, 'click', function (e) {
-    t.ContainerChangeStatus.set('open', false);
-});
+                on(t.DialogCStatusOK, 'click', function (e) {
+                 t.uDCChangeStatus.Insert().then(function(){
+                     t.uDCChangeStatus.Clear();
+                     t.ContainerChangeStatus.set('open', false);
+                 }); 
+             });
 
-on(t.DialogCStatusOK, 'click', function (e) {
-   t.uDCChangeStatus.Insert().then(function(){
-       t.uDCChangeStatus.Clear();
-       t.ContainerChangeStatus.set('open', false);
-   }); 
-                  // popup.close(t.DialogChangeStatus);
-              });
-
-
-
-
-
-
-
-},
+            },
 
         /**
  * Accept decimal input and return issue an error message.
@@ -63,34 +54,17 @@ on(t.DialogCStatusOK, 'click', function (e) {
     var t = this;
     console.debug(_v);
     
-
-    t.uDCChangeStatus.setField('idevent', _v.idevent);   
-
-    //t.set('idevent', _v.idevent);
     t._set_and_hide_show_field('idevent', _v.idevent);
-    
-    //t.set('dateevent', new Date(_v.dateevent).toLocaleString());
     t._set_and_hide_show_field('dateevent', new Date(_v.dateevent).toLocaleString());
-
-    //t.set('priority', _v.priority);
     t._set_and_hide_show_field('priority', _v.priority);
-
-    //t.set('description', _v.description);
     t._set_and_hide_show_field('description', _v.description);
-
-    //t.set('status', _v.label_status);
     t._set_and_hide_show_field('status', _v.label_status);
-
-    //t.set('equipment', _v.equipment);
     t._set_and_hide_show_field('equipment', _v.equipment);
-
-
-    //t.set('eventtype', _v.label_eventtype);
     t._set_and_hide_show_field('eventtype', _v.label_eventtype);
 
     t.SelectEventStatus.set('value', _v.ideventstatustype);
     t.TEComments.Grid.Select({idevent: _v.idevent});
-
+    t.uDCChangeStatus.setField('idevent', _v.idevent);   
 
 
 },
@@ -105,19 +79,17 @@ _set_and_hide_show_field: function(field, value){
 
                 if(value && value.length > 0){
 
- //t.set(field, value);    
- f.innerHTML = value;
- domStyle.set(fielset, 'display', 'inline-block');
+                 f.innerHTML = value;
+                 domStyle.set(fielset, 'display', 'inline-block');
 
-}else{
- //t.set(field, value);    
- f.innerHTML = value;
- domStyle.set(fielset, 'display', 'none');
-}
+             }else{
+                 f.innerHTML = value;
+                 domStyle.set(fielset, 'display', 'none');
+             }
 
-}
+         }
 
-});
+     });
 
 
 
