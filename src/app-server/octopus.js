7c3fefb7e25c3bc4fc916fea9d19590be6b70dfa
edwinspire <edwinspire@gmail.com>
2017-02-26 10:43:11 -0500

    Modificamos Octopus

--- src/app-server/octopus.js
+++ src/app-server/octopus.js
@@ -35,17 +35,75 @@ require(["dojo/request",
 
 var ConfigParameter = new Config();
 
-console.log(ConfigParameter);
+
+
+function ConnPV(ip){
+
+mssql.connect({
+    user: 'sa',
+    password: 'sqlsa',
+    server: ip, // You can use 'localhost\\instance' to connect to named instance 
+    database: 'msdb',
+    options: {
+        encrypt: true // Use this if you're on Windows Azure 
+    }
+}).then(function() {
+    // Query 
+var msSQLConnectionPV = this;    
+
+var srtquery = `
+DECLARE @Fecha AS varchar(10)
+SET @Fecha = '2017/02/24'
+
+SELECT count(*) AS CanTotal, 'Registros' AS TipoTrx
+FROM [ITE_Pos].[fact].[tbl_MovimientoInventario_cab] c(nolock) 
+INNER JOIN [ITE_Pos].[fact].[tbl_MovimientoInventario_det] d(nolock) on c.num_mov = d.num_mov AND c.tipo_mov = d.tipo_mov and c.serie_factura = d.serie_factura
+WHERE convert(varchar,c.FechaRegistro,111) = @Fecha AND c.tipo_mov IN ('TRASPASOS','STRASPASOS','AJUSMAS','AJUSMEN','I') and c.num_mov <> 1
+UNION ALL
+SELECT sum(d.cantidad), 'Entradas'
+FROM [ITE_Pos].[fact].[tbl_MovimientoInventario_cab] c(nolock) 
+INNER JOIN [ITE_Pos].[fact].[tbl_MovimientoInventario_det] d(nolock) ON c.num_mov = d.num_mov AND c.tipo_mov = d.tipo_mov and c.serie_factura = d.serie_factura
+WHERE convert(varchar,c.FechaRegistro,111) = @Fecha AND c.tipo_mov IN ('TRASPASOS','AJUSMAS','I') and c.num_mov <> 1
+UNION ALL
+SELECT sum(d.cantidad),'Salidas' FROM [ITE_Pos].[fact].[tbl_MovimientoInventario_cab] c(nolock) 
+INNER JOIN [ITE_Pos].[fact].[tbl_MovimientoInventario_det] d(nolock) ON c.num_mov = d.num_mov AND c.tipo_mov = d.tipo_mov and c.serie_factura = d.serie_factura
+WHERE convert(varchar,c.FechaRegistro,111) = @Fecha AND c.tipo_mov IN ('STRASPASOS','AJUSMEN') and c.num_mov <> 1
+`;
+
+    new mssql.Request()
+ //   .input('input_parameter', sql.Int, value);
+    .query(srtquery).then(function(recordset) {
+   
+
+msSQLConnectionPV.close();
+
+        console.dir(recordset);
+
+
+    }).catch(function(err) {
+        // ... error checks 
+        console.log(err);
+    });
+ 
+
+}).catch(function(err) {
+    // ... error checks 
+    console.log(err);
+});
+
+}
+
+
 
 		var PostgreSQL = new pgOMS(ConfigParameter.pgConnectionParameters);
 
 
-		setInterval(function(){
-			console.log('Tareas periodicas');
-			PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact;", []).then(function(response){
-				console.log(response);
-			});
-		}, 60*1000);
+		// setInterval(function(){
+		// 	console.log('Tareas periodicas');
+		// 	PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact;", []).then(function(response){
+		// 		console.log(response);
+		// 	});
+		// }, 60*1000);
 
 
 
@@ -113,6 +171,21 @@ msSQLConnection.close();
 
 console.log(MovInvMatriz);
 
+			PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact;", []).then(function(response){
+				console.log(response);
+
+ //var MovInvPV = new Memory({data:response.rows, idProperty: 'account'});
+ //console.log(MovInvPV);
+array.forEach(response.rows, function(oficina){
+
+ConnPV(oficina.ip);
+
+});
+
+
+
+			});
+
 
 
     }).catch(function(err) {
@@ -134,3 +207,10 @@ process.on('uncaughtException', function(error){
 
 
 });
+
+
+
+
+
+
+
