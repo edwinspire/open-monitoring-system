03a857f44e7cdbfdbc633ed9cf88995e4d9789a3
edwinspire <edwinspire@gmail.com>
2017-02-26 20:39:39 -0500

    Revision Octopus

--- src/app-server/octopus.js
+++ src/app-server/octopus.js
@@ -34,14 +34,16 @@ require(["dojo/request",
 
 
 var ConfigParameter = new Config();
+var MovInvMatriz = new Memory();
+var MovInvConsolidado = new Memory();
 
 
 
-function ConnPV(ip){
+function ConnPV(ip, oficina){
 
 mssql.connect({
     user: 'sa',
-    password: 'sqlsa',
+    password: 'sql',
     server: ip, // You can use 'localhost\\instance' to connect to named instance 
     database: 'msdb',
     options: {
@@ -74,10 +76,23 @@ WHERE convert(varchar,c.FechaRegistro,111) = @Fecha AND c.tipo_mov IN ('STRASPAS
  //   .input('input_parameter', sql.Int, value);
     .query(srtquery).then(function(recordset) {
    
+console.log(oficina, ip);
+//msSQLConnectionPV.close();
 
-msSQLConnectionPV.close();
+        //console.dir(recordset);
+        array.forEach(recordset, function(item){
 
-        console.dir(recordset);
+console.log(item);
+MovInvMatriz.filter({oficina: oficina, TipoTrx: item.TipoTrx}).forEach(function(reg){
+//console.log("== ", reg);
+var nItem = reg;
+nItem["CanTotalPV"] = item.CanTotal;
+console.log("==> ", nItem);
+MovInvMatriz.put(nItem);
+});
+
+
+        });
 
 
     }).catch(function(err) {
@@ -98,35 +113,21 @@ msSQLConnectionPV.close();
 		var PostgreSQL = new pgOMS(ConfigParameter.pgConnectionParameters);
 
 
-		// setInterval(function(){
-		// 	console.log('Tareas periodicas');
-		// 	PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact;", []).then(function(response){
-		// 		console.log(response);
-		// 	});
-		// }, 60*1000);
-
-
-
 		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 // create reusable transporter object using the default SMTP transport 
 var transporter = nodeMailer.createTransport(ConfigParameter.smtpConfig);
 
-/*
-// send mail with defined transport object 
-transporter.sendMail(ConfigParameter.mailOptions, function(error, info){
-	if(error){
-		return console.log(error);
-	}
-	console.log('Message email sent: ' + info.response);
-});
-*/
+
+
+
 
 
 mssql.connect({
     user: 'sa',
-    password: 'sqlsa',
-    server: '192.168.238.10', // You can use 'localhost\\instance' to connect to named instance 
+    password: 'sql',
+    server: '172.16.200.12', // You can use 'localhost\\instance' to connect to named instance 
     database: 'msdb',
+    requestTimeout: 3000000,
     options: {
         encrypt: true // Use this if you're on Windows Azure 
     }
@@ -135,10 +136,11 @@ mssql.connect({
 var msSQLConnection = this;    
 
 var srtquery = `
+USE EasyGestionEmpresarial
 DECLARE @Fecha AS varchar(10)
 SET @Fecha = '2017/02/24'
 
-SELECT c.oficina, count(*) AS CanTotal, '#Registros' AS TipoTrx
+SELECT c.oficina, count(*) AS CanTotal, 'Registros' AS TipoTrx
 FROM tbl_maestromovinvent c(nolock) 
 INNER JOIN tbl_movinvent d(nolock) ON c.num_mov = d.num_mov AND c.tipo_mov = d.tipo_mov and c.oficina = d.oficina and c.serie_factura = d.serie_factura
 WHERE convert(varchar,c.FechaRegistro,111) = @Fecha AND c.tipo_mov in ('TRASPASOS','STRASPASOS','AJUSMAS','AJUSMEN','I') and c.num_mov <> 1
@@ -162,26 +164,56 @@ GROUP BY c.oficina ORDER BY c.oficina, TipoTrx
     .query(srtquery).then(function(recordset) {
    
 
-msSQLConnection.close();
+//msSQLConnection.close();
 
-        console.dir(recordset);
+  //      console.dir(recordset);
 
 
- var MovInvMatriz = new Memory({data:recordset, idProperty: 'oficina'});
+ MovInvMatriz = new Memory({data:recordset});
 
-console.log(MovInvMatriz);
+//console.log(MovInvMatriz);
 
-			PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact;", []).then(function(response){
-				console.log(response);
+			PostgreSQL.query("SELECT accounts.idcontact, accounts.enabled AS enabled_account, (accounts.last_name ||' '|| accounts.first_name) as account_name, accounts.identification,   accounts.idaccountstate,   accounts.account,   accounts.iddivision,   network_devices.idequipment,   network_devices.equipment,   network_devices.ip,   network_devices.mac,   network_devices.username,   network_devices.pwd,   network_devices.port,   network_devices.monitored,   network_devices.report_validator FROM   public.accounts,   public.network_devices WHERE   network_devices.idaccount = accounts.idcontact AND iddivision = 6;", []).then(function(response){
+			//	console.log(response);
 
  //var MovInvPV = new Memory({data:response.rows, idProperty: 'account'});
  //console.log(MovInvPV);
 array.forEach(response.rows, function(oficina){
 
-ConnPV(oficina.ip);
+ConnPV(oficina.ip, oficina.account);
 
 });
 
+
+
+
+setTimeout(function(){
+
+var Resultado = "";
+
+//console.log(MovInvMatriz);
+MovInvMatriz.forEach(function(mov){
+Resultado = Resultado+mov.oficina+"|\t"+mov.CanTotal+"|\t"+mov.CanTotalPV+"|\t"+mov.TipoTrx+"¡</br>\n\r";
+
+});
+
+
+	mailOptions = {
+    from: '"Edwin De La Cruz 👥" <edwindelacruz@farmaenlace.com>', // sender address 
+    to: 'edwinspire@gmail.com, edwindelacruz@farmaenlace.com', // list of receivers 
+    subject: 'Open Monitoring System Start ✔ '+Date.now(), // Subject line 
+    text: "Revision Movimientos Inventarios ETA", // plaintext body 
+    html: Resultado// html body 
+};
+// send mail with defined transport object 
+transporter.sendMail(mailOptions, function(error, info){
+	if(error){
+		return console.log(error);
+	}
+	console.log('Message email sent: ' + info.response);
+});
+
+}, 20000);
 
 
 			});
