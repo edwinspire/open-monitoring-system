fdf03f5de8cee428267865777d07bb33dbbb2934
edwinspire <edwinspire@gmail.com>
2017-03-05 14:17:55 -0500

    Se cambia udc_table_account_equipments.js por las modificaciones realizadas en oms.js

--- src/app-server/api/postgres/udc_table_account_equipments.js
+++ src/app-server/api/postgres/udc_table_account_equipments.js
@@ -18,7 +18,7 @@ udc_table_account_equipments: function(req, res){
 			w["idaccount"] = post.idaccount;
 
 			qp = t.Select(post.UdcTable, []).whereAnd([w]).build();
-			t.response_query(req, res, qp.query, qp.param);
+			t.response_query(res, qp.query, qp.param);
 			break;
 			case 'update':
 			var w = {};
@@ -26,7 +26,7 @@ udc_table_account_equipments: function(req, res){
 			w[post.UdcIdProperty] = post[post.UdcIdProperty];
 
 			qp = t.Update(post.UdcTable, post, []).whereAnd([w], ["UdcAction", "UdcRowFingerPrint", "UdcRowFingerPrintValue", "UdcTable", "UdcIdProperty"]).build();
-			t.response_update(req, res, qp.query, qp.param);
+			t.response_update(res, qp.query, qp.param);
 			break;	
 			case 'insert':
 			var w = {};
@@ -34,20 +34,19 @@ udc_table_account_equipments: function(req, res){
 			w[post.UdcIdProperty] = post[post.UdcIdProperty];
 
 			qp = t.Insert(post.UdcTable, post, []).build();
-			t.response_insert(req, res, qp.query, qp.param);
+			t.response_insert(res, qp.query, qp.param);
 			break;			
 			default:
-			res.status(500).json({success: false, data: "Intentando una accion invalida "+post.UdcAction, req: post});
-	//return false;
-	break;
+			res.status(500).json({success: false, data: "Intentando una accion invalida "+post.UdcAction, req: post});=
+			break;
 
-}
-
+		}
 
 
-}else{
-	res.status(500).json({success: false, data: "No ha definido una tabla a buscar"});
-}
+
+	}else{
+		res.status(500).json({success: false, data: "No ha definido una tabla a buscar"});
+	}
 
 
 }              
