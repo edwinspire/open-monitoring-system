bdc79f057b8d0bc76ea7c227e768d9d61633cf9f
edwinspire <edwinspire@gmail.com>
2017-03-05 14:11:10 -0500

    Se elimina TableChangedEvent.js no se lo esta usando

--- src/app-server/api/config.js
+++ src/app-server/api/config.js
@@ -12,15 +12,3 @@ process.env.SMPT_SECURE = true;
 process.env.SMPT_AUTH_USER = 'user';
 process.env.SMPT_AUTH_PWD = 'password';
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
-
-//this.ServerPort = 8080;
-//this.pgConnectionParameters = {user: 'postgres', pwd: 'pg4321', host: 'localhost', db: 'oms'};
-//this.smtpConfig = {host: 'mail.localhost.com', port: 465, ignoreTLS:false, secure: true, auth: {user: 'user', pass: 'password'}};
-// this.mailOptions = {
-//     from: '"Edwin De La Cruz 👥" <edwindelacruz@farmaenlace.com>', // sender address 
-//     to: 'edwinspire@gmail.com', // list of receivers 
-//     subject: 'Open Monitoring System Start ✔ '+Date.now(), // Subject line 
-//     text: 'El servidor ha sido iniciado en el puerto '+this.ServerPort, // plaintext body 
-//     html: '<b>El servidor ha sido iniciado en el puerto '+this.ServerPort+'</b>' // html body 
-// };
