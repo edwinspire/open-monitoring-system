3550eed5c427903758a2aa4f1f8400ec97b55e40
edwinspire <edwinspire@gmail.com>
2017-03-04 22:53:51 -0500

    Se agrega modulo ping para octopus

---
+++ src/app-server/api/custom/custom.js
@@ -0,0 +1,24 @@
+/**
+     * Custom Module
+     *
+     * @module 
+     */
+     define(['dojo/_base/declare'
+     	], function (declare) {
+
+     		return declare('custom', Evented, {
+
+propertyX: 0,
+
+//////////////////////////////////
+// The constructor
+constructor: function(args) {
+	dojo.safeMixin(this,args);
+var t = this;
+
+}
+
+
+
+});
+});
