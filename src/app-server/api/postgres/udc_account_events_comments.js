7582f550851c378f673ae1a5e07214446771b1c7
edwinspire <edwinspire@gmail.com>
2017-03-05 14:13:18 -0500

    Se modifica udc_account_events_comments.js por los cambios en los argumentos de oms.js

--- src/app-server/api/postgres/udc_account_events_comments.js
+++ src/app-server/api/postgres/udc_account_events_comments.js
@@ -16,48 +16,35 @@ udc_account_events_comments: function(req, res){
 			case 'select_rows':
 			var w = {};
 			w["idevent"] = post.idevent;
-			//w["isopen"] = true;
 
 			qp = t.Select('event_comments', []).whereAnd([w]).orderBy(' ideventcomment ').build();
-			t.response_query(req, res, qp.query, qp.param);
+			t.response_query(res, qp.query, qp.param);
 			break;
 			case 'selectxxxx':
 			var w = {};
 			w["idevent"] = post.idevent;
-			//w["isopen"] = true;
 
 			qp = t.Select('view_events_isopen', []).whereAnd([w]).orderBy(' dateevent ').build();
-			t.response_query(req, res, qp.query, qp.param);
+			t.response_query(res, qp.query, qp.param);
 			break;
-			/*
-			case 'update':
-			var w = {};
-			w[post.UdcRowFingerPrint] = post.UdcRowFingerPrintValue;
-			w[post.UdcIdProperty] = post[post.UdcIdProperty];
 
-			qp = t.Update(post.UdcTable, post, []).whereAnd([w], ["UdcAction", "UdcRowFingerPrint", "UdcRowFingerPrintValue", "UdcTable", "UdcIdProperty"]).build();
-			t.response_update(req, res, qp.query, qp.param);
-			break;	*/
 			case 'insert':
 			var w = {};
-			//w[post.UdcRowFingerPrint] = post.UdcRowFingerPrintValue;
-			//w[post.UdcIdProperty] = post[post.UdcIdProperty];
 
 			qp = t.Insert('event_comments', post, []).build();
-			t.response_insert(req, res, qp.query, qp.param);
+			t.response_insert(res, qp.query, qp.param);
 			break;		
 			default:
 			res.status(500).json({success: false, data: "Intentando una accion invalida "+post.UdcAction, req: post});
-	//return false;
-	break;
+			break;
 
-}
-
+		}
 
 
-}else{
-	res.status(500).json({success: false, data: "No ha definido una tabla a buscar"});
-}
+
+	}else{
+		res.status(500).json({success: false, data: "No ha definido una tabla a buscar"});
+	}
 
 
 }              
