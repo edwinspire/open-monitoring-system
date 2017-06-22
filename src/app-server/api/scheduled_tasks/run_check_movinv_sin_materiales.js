86d4cc4af76136c09bc99759d3a480feb80d6bd3
edwinspire <edwinspire@gmail.com>
2017-06-22 17:30:24 -0500

    .

--- src/app-server/api/scheduled_tasks/run_check_movinv_sin_materiales.js
+++ src/app-server/api/scheduled_tasks/run_check_movinv_sin_materiales.js
@@ -29,7 +29,7 @@ run_check_movinv_sin_materiales: function(task){
     new mssql.Request(cnxmatriz)
     .query(srtquery).then(function(recordset) {
 
-//    	console.log(recordset);
+    	console.log(recordset);
 var ideventtype = task.task_parameters.ideventtype_on_alarm;
 var materiales = [];
 
