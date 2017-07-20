1a048b8b33bb629ea572fa736fdbadf75f8f4c8d
edwinspire <edwinspire@gmail.com>
2017-07-20 15:20:23 -0500

    Cambios

--- src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
+++ src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
@@ -4,9 +4,9 @@ require(["dojo/_base/lang", "api/scheduled_tasks/scheduled_tasks",   "dojo/Defer
 	lang.extend(Octopus, {
 /////////////////////////////////////////
 run_resumen_facturacion_electronica: function(task){
-	
-	var deferred = new Deferred();
-	var t = this;
+
+    var deferred = new Deferred();
+    var t = this;
     t._run_resumen_facturacion_electronica(task.task_parameters).then(function(){
         deferred.resolve(true);
     }, function(){
@@ -64,55 +64,62 @@ mssql.connect(config).then((cnx) => {
 
             var res = recordset[0];
             var enviado = "Ningun enviado encontrado en la tabla";
-            var procesado = "Ningun procesado encontrado en la tabla";
+            var procesado = 'Ninguno "En Porceso" encontrado en la tabla';
             var priority = 99;
             var eventtype = param.ideventtype_on_restore;
+            try{
 
-            if(res.QueryOn > 0){
+                if(res.QueryOn > 0){
 
-                priority = 6;
-                eventtype = param.ideventtype_on_alarm;
+                    priority = 6;
+                    eventtype = param.ideventtype_on_alarm;
 
-            }else if(res.pendientes > 0){
+                }else if(res.pendientes > 0){
 
-                if(res.ultimo_enviado){
-                    var tiempo_sin_enviar = DojoDate.difference(res.ultimo_enviado, new Date(), "minute");
-                    if(tiempo_sin_enviar >= res.tiempo_max_sin_enviar){
-                       priority = 1;
-                       eventtype = param.ideventtype_on_alarm;
-                   }else{
-                    priority = 8;
-                    eventtype = param.ideventtype_on_restore;
-                }    
+                    if(res.ultimo_enviado){
+                        var tiempo_sin_enviar = DojoDate.difference(res.ultimo_enviado, new Date(), "minute");
+                        if(tiempo_sin_enviar >= res.tiempo_max_sin_enviar){
+                         priority = 1;
+                         eventtype = param.ideventtype_on_alarm;
+                     }else{
+                        priority = 7;
+                        eventtype = param.ideventtype_on_restore;
+                    }    
+                }
+
+            }else{
+                priority = 10;
+                eventtype = param.ideventtype_on_restore;
             }
 
-        }else{
-            priority = 10;
-            eventtype = param.ideventtype_on_restore;
+            if(res.ultimo_procesado){
+                procesado = dateLocale.format( res.ultimo_procesado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+            }
+
+            if(res.ultimo_enviado){
+                enviado = dateLocale.format( res.ultimo_enviado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+            }
+
+            var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+procesado+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+enviado+"</br><b>QueryOn: </b>"+res.queryon;
+            var event = {idaccount: 0, ideventtype: eventtype, source: t.textToMD5(param.ip), description: descr, details: {iddivision: param.iddivision}, priority: priority};
+
+            t.send_event_pg(event, []).then(function(result){
+
+                deferred.resolve(true);
+
+            }, function(err){
+                console.log(err);
+                deferred.resolve(true);
+            });
+
+        }catch(error){
+            console.trace(error);
+            deferred.resolve(error);
         }
 
-        if(res.ultimo_procesado){
-            procesado = dateLocale.format( res.ultimo_procesado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
-        }
-
-        if(res.ultimo_enviado){
-            enviado = dateLocale.format( res.ultimo_enviado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
-        }
-
-        var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+procesado+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+enviado+"</br><b>QueryOn: </b>"+res.queryon;
-        var event = {idaccount: 0, ideventtype: eventtype, source: t.textToMD5(param.ip), description: descr, details: {iddivision: param.iddivision}, priority: priority};
-
-        t.send_event_pg(event, []).then(function(result){
-
-            deferred.resolve(true);
-
-        }, function(err){
-            console.log(err);
-            deferred.resolve(true);
-        });
     }else{
-     deferred.resolve(true);
- }
+       deferred.resolve(true);
+   }
 
 
 }).catch(function(err) {
@@ -121,8 +128,8 @@ mssql.connect(config).then((cnx) => {
 });
 
 }).catch(err => {
- console.trace(err);
- deferred.resolve(true);
+   console.trace(err);
+   deferred.resolve(true);
 })
 
 return deferred.promise;
