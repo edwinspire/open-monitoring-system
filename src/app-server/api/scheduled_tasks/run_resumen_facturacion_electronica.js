40add0a5c6ea401c7768c3d267184c8e19ed4fc6
edwinspire <edwinspire@gmail.com>
2017-07-20 12:12:11 -0500

    Cambios en el query para obtener los campos de fecha en el formato correcto con timezone ya que en javascript se lo maneja de ese modo.

--- src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
+++ src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
@@ -1,6 +1,6 @@
 // Dojo 1.7+ (AMD)
 "dojo/promise/all"
-require(["dojo/_base/lang", "api/scheduled_tasks/scheduled_tasks",   "dojo/Deferred", "dojo/_base/array", "dojo/promise/all", "dojo/on", "dojo/node!mssql"], function(lang, Octopus, Deferred, array, all, on, mssql){
+require(["dojo/_base/lang", "api/scheduled_tasks/scheduled_tasks",   "dojo/Deferred", "dojo/_base/array", "dojo/date/locale", "dojo/on", "dojo/node!mssql"], function(lang, Octopus, Deferred, array, dateLocale, on, mssql){
 	lang.extend(Octopus, {
 /////////////////////////////////////////
 run_resumen_facturacion_electronica: function(task){
@@ -21,28 +21,27 @@ _run_resumen_facturacion_electronica: function(param){
 
     var srtquery = `USE facturacionelectronica; 
 
-SELECT (SELECT Count(*) 
-        FROM   pv_xmlfacturacionelectronica (nolock) 
-        WHERE  serie_factura NOT IN (SELECT den_serie_factura 
-                                     FROM   ge_tmp_documentosenviados (nolock) 
-                                     WHERE  den_tipo_documento IN ( 
-                                            '01', '04', '06' )) 
-       )                                                        AS pendientes, 
-       Cast((SELECT TOP(1) dpr_fecha_proceso 
-             FROM   gen_tmp_documentosprocesados 
-             ORDER  BY dpr_fecha_proceso DESC) AS DATETIME2)    AS 
-       ultimo_procesado, 
-       (SELECT Count(*) 
-        FROM   gen_tmp_documentosprocesados 
-        WHERE  dpr_estado_proceso = 'En Proceso')               AS en_proceso, 
-       CONVERT(VARCHAR, (SELECT TOP (1) glp_fecha_inicio 
-                         FROM   gen_log_controlprocesos 
-                         WHERE  glp_estado = 'QueryOn' 
-                         ORDER  BY glp_fecha_inicio DESC), 120) AS queryon, 
-       Cast((SELECT TOP(1) den_fecha_envio 
-             FROM   ge_tmp_documentosenviados 
-             ORDER  BY den_fecha_envio DESC) AS DATETIME2)      AS 
-       ultimo_enviado; `;
+    SELECT (SELECT Count(*) 
+    FROM   pv_xmlfacturacionelectronica (nolock) 
+    WHERE  serie_factura NOT IN (SELECT den_serie_factura 
+    FROM   ge_tmp_documentosenviados (nolock) 
+    WHERE  den_tipo_documento IN ( 
+    '01', '04', '06' )) 
+    )                                                          AS pendientes, 
+    Todatetimeoffset((SELECT TOP(1) dpr_fecha_proceso 
+    FROM   gen_tmp_documentosprocesados 
+    ORDER  BY dpr_fecha_proceso DESC), -300) AS 
+    ultimo_procesado, 
+    (SELECT Count(*) 
+    FROM   gen_tmp_documentosprocesados 
+    WHERE  dpr_estado_proceso = 'En Proceso')                 AS en_proceso, 
+    (SELECT Count(*) glp_fecha_inicio 
+    FROM   gen_log_controlprocesos 
+    WHERE  glp_estado = 'QueryOn')                            AS queryon, 
+    Todatetimeoffset((SELECT TOP(1) den_fecha_envio 
+    FROM   ge_tmp_documentosenviados 
+    ORDER  BY den_fecha_envio DESC), -300)   AS 
+    ultimo_enviado;`;
 
     var config = {
         user: param.user,
@@ -64,7 +63,16 @@ mssql.connect(config).then((cnx) => {
         if(recordset.length > 0){
 
             var res = recordset[0];
-            var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+res.ultimo_procesado.toISOString()+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+res.ultimo_enviado.toISOString()+"</br><b>QueryOn: </b>"+res.queryon;
+            var procesado = "Ningun procesado encontrado en la tabla";
+            if(res.ultimo_procesado){
+                procesado = dateLocale.format( res.ultimo_procesado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+            }
+            var enviado = "Ningun enviado encontrado en la tabla";
+            if(res.ultimo_procesado){
+                enviado = dateLocale.format( res.ultimo_enviado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+            }
+
+            var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+procesado+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+enviado+"</br><b>QueryOn: </b>"+res.queryon;
             var event = {idaccount: 0, ideventtype: param.ideventtype_on_alarm, source: t.textToMD5(param.ip), description: descr, details: {iddivision: param.iddivision}, priority: 2};
 
             t.send_event_pg(event, []).then(function(result){
