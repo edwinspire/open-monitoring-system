cd7d8e5d6c928dd14a37fde5d1be8563c9540857
edwinspire <edwinspire@gmail.com>
2017-07-20 12:37:54 -0500

    Cambios en las prioridades

--- src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
+++ src/app-server/api/scheduled_tasks/run_resumen_facturacion_electronica.js
@@ -1,6 +1,6 @@
 // Dojo 1.7+ (AMD)
 "dojo/promise/all"
-require(["dojo/_base/lang", "api/scheduled_tasks/scheduled_tasks",   "dojo/Deferred", "dojo/_base/array", "dojo/date/locale", "dojo/on", "dojo/node!mssql"], function(lang, Octopus, Deferred, array, dateLocale, on, mssql){
+require(["dojo/_base/lang", "api/scheduled_tasks/scheduled_tasks",   "dojo/Deferred", "dojo/_base/array", "dojo/date/locale", "dojo/date", "dojo/node!mssql"], function(lang, Octopus, Deferred, array, dateLocale, DojoDate, mssql){
 	lang.extend(Octopus, {
 /////////////////////////////////////////
 run_resumen_facturacion_electronica: function(task){
@@ -63,39 +63,66 @@ mssql.connect(config).then((cnx) => {
         if(recordset.length > 0){
 
             var res = recordset[0];
-            var procesado = "Ningun procesado encontrado en la tabla";
-            if(res.ultimo_procesado){
-                procesado = dateLocale.format( res.ultimo_procesado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
-            }
             var enviado = "Ningun enviado encontrado en la tabla";
-            if(res.ultimo_procesado){
-                enviado = dateLocale.format( res.ultimo_enviado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+            var procesado = "Ningun procesado encontrado en la tabla";
+            var priority = 99;
+            var eventtype = param.ideventtype_on_restore;
+
+            if(res.QueryOn > 0){
+
+                priority = 6;
+                eventtype = param.ideventtype_on_alarm;
+
+            }else if(res.pendientes > 0){
+
+                if(res.ultimo_enviado){
+                    var tiempo_sin_enviar = DojoDate.difference(res.ultimo_enviado, new Date(), "minute");
+                    if(tiempo_sin_enviar >= res.tiempo_max_sin_enviar){
+                     priority = 1;
+                     eventtype = param.ideventtype_on_alarm;
+                 }else{
+                    priority = 8;
+                    eventtype = param.ideventtype_on_restore;
+                }    
             }
 
-            var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+procesado+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+enviado+"</br><b>QueryOn: </b>"+res.queryon;
-            var event = {idaccount: 0, ideventtype: param.ideventtype_on_alarm, source: t.textToMD5(param.ip), description: descr, details: {iddivision: param.iddivision}, priority: 2};
-
-            t.send_event_pg(event, []).then(function(result){
-
-                deferred.resolve(true);
-
-            }, function(err){
-                console.log(err);
-                deferred.resolve(true);
-            });
         }else{
-         deferred.resolve(true);
-     }
+            priority = 10;
+            eventtype = param.ideventtype_on_restore;
+        }
+
+        if(res.ultimo_procesado){
+            procesado = dateLocale.format( res.ultimo_procesado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+        }
+
+        if(res.ultimo_procesado){
+            enviado = dateLocale.format( res.ultimo_enviado, {selector: 'date', datePattern: "dd-MM-yyyy HH:mm:ss"});
+        }
+
+        var descr = "<b>Pendientes: </b>"+res.pendientes+"</br><b>Ultimo procesado: </b>"+procesado+"</br><b>En Proceso: </b>"+res.en_proceso+"</br><b>Ultimo Enviado: </b>"+enviado+"</br><b>QueryOn: </b>"+res.queryon;
+        var event = {idaccount: 0, ideventtype: eventtype, source: t.textToMD5(param.ip), description: descr, details: {iddivision: param.iddivision}, priority: priority};
+
+        t.send_event_pg(event, []).then(function(result){
+
+            deferred.resolve(true);
+
+        }, function(err){
+            console.log(err);
+            deferred.resolve(true);
+        });
+    }else{
+       deferred.resolve(true);
+   }
 
 
- }).catch(function(err) {
+}).catch(function(err) {
     console.trace(err);
     deferred.resolve(true);
 });
 
 }).catch(err => {
- console.trace(err);
- deferred.resolve(true);
+   console.trace(err);
+   deferred.resolve(true);
 })
 
 return deferred.promise;
