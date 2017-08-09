4a2b7da14ad8b378184380517d036f373ca59372
edwinspire <edwinspire@gmail.com>
2017-08-09 06:25:14 -0500

    Cambios

--- src/app-server/server.js
+++ src/app-server/server.js
@@ -40,8 +40,8 @@ require(["api/express/express",
   ], function(express, on, array, os, cluster, crypto, socketIO, path, fs, LogSystem, https, url, pG, nodeMailer, pgOMS, MD5, Config, sessionusers){
 
 
-    var Log = new LogSystem('debug', fs.createWriteStream('Log_oms'+(new Date()).toLocaleDateString()+'.log'));
-    Log.debug("Inicia Open Monitoring System WebApp");
+    //var Log = new LogSystem('debug', fs.createWriteStream('Log_oms'+(new Date()).toLocaleDateString()+'.log'));
+    //Log.debug("Inicia Open Monitoring System WebApp");
 
     const numCPUs = os.cpus().length;
 
@@ -52,33 +52,33 @@ require(["api/express/express",
 PostgreSQL.get_config_from_db().then(function(){
 
   PostgreSQL._schema_gui_properties_fromdb().then(function(r){
-      //Log.debug(r);
+      ////Log.debug(r);
     });
 
   setInterval(function(){
 
     PostgreSQL.get_change_in_tables().then(function(){
-  //Log.debug('Se busca cambios en las tablas');
+  ////Log.debug('Se busca cambios en las tablas');
 });
 
   }, 3*1000);
 
 
   setInterval(function(){
-    Log.debug('Tareas periodicas');
+    //Log.debug('Tareas periodicas');
     PostgreSQL.query("SELECT * FROM events.fun_set_expired_events();", []).then(function(response){
-      //Log.debug(response);
+      ////Log.debug(response);
     });
     PostgreSQL.query("SELECT * FROM gui.fun_remove_notifications_old();", []).then(function(response){
-      //Log.debug(response);
+      ////Log.debug(response);
     });
     PostgreSQL.query("SELECT * FROM fun_last_modified_table_remove_olds();", []).then(function(response){
-      //Log.debug(response);
+      ////Log.debug(response);
     });
 
 // Esto debe dispararse solo al inicio de la aplicacion y cuando haya cambios en las tablas involucradas
 PostgreSQL._schema_gui_properties_fromdb().then(function(r){
-  Log.debug(r);
+  //Log.debug(r);
 });
 }, 15*1000);
 
@@ -98,7 +98,7 @@ PostgreSQL._schema_gui_properties_fromdb().then(function(r){
 
 
   var cnxSMTP = {host: process.env.SMPT_HOST, port: process.env.SMPT_PORT, ignoreTLS: process.env.SMPT_IGNORETLS, secure: process.env.SMPT_SECURE, auth: {user: process.env.SMPT_AUTH_USER, pass: process.env.SMPT_AUTH_PWD}};
-  Log.debug(cnxSMTP);
+  //Log.debug(cnxSMTP);
 // create reusable transporter object using the default SMTP transport 
 var transporter = nodeMailer.createTransport(cnxSMTP);
 
@@ -108,9 +108,9 @@ PostgreSQL.configuration_server.query({config_name: "mailOptions"}).forEach(func
 
   transporter.sendMail(config.configuration, function(error, info){
     if(error){
-      return Log.debug(error);
+      return //Log.debug(error);
     }
-    Log.debug('Message email sent: ' + info.response);
+    //Log.debug('Message email sent: ' + info.response);
   });
 });
 
@@ -127,11 +127,11 @@ var sio = socketIO.listen(webServer);
 
 var pgEventTs = PostgreSQL.on('tschange', function(r){
 
-  Log.debug(r);
+  //Log.debug(r);
 
   if(r.table_name == 'gui.column_properties' || r.table_name == 'gui.tables_view_properties'){
     PostgreSQL._schema_gui_properties_fromdb().then(function(rx){
-      Log.debug('GUI ha cambiado');
+      //Log.debug('GUI ha cambiado');
     }); 
   }
 
@@ -211,12 +211,12 @@ clientio.on('clogin',function(config){
 
 
 clientio.on('disconnect',function(){
-  Log.debug('Server has disconnected');
+  //Log.debug('Server has disconnected');
 
 });
 
 clientio.on('reconnect', function() {
-  Log.debug('reconnect fired!');
+  //Log.debug('reconnect fired!');
   console.log('reconnect fired!');
 });
 
@@ -225,21 +225,21 @@ clientio.on('reconnect', function() {
 
 
 sessionUsers.on('newsession', function(e){
-  Log.debug(e);
+  //Log.debug(e);
   sio.emit('command', {command: 'heartbeat'});    
 });
 
 
 
 webServer.listen(process.env.PORT, function(){
-  Log.debug("Listening on " + process.env.PORT);
+  //Log.debug("Listening on " + process.env.PORT);
 });
 
 
 
 
 var pgEventError = PostgreSQL.on('pgError', function(error){
-  Log.debug(error);
+  //Log.debug(error);
 });
 
 
@@ -247,7 +247,7 @@ var pgEventError = PostgreSQL.on('pgError', function(error){
 
 
 process.on('uncaughtException', function(error){
-  Log.debug(error);
+  //Log.debug(error);
 });
 
 
