c6e602bab39e82dda170428ca050cba67b1be7a7
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2020-04-27 07:38:27 -0500

    Deploy

---
+++ openmonitoringsystem/opt/openmonitoringsystem/frontend/src/themes/black/theme.ts
@@ -0,0 +1,75 @@
+import * as accordionPane from './@dojo/widgets/accordion-pane/accordion-pane.m.css';
+import * as button from './@dojo/widgets/button/button.m.css';
+import * as calendar from './@dojo/widgets/calendar/calendar.m.css';
+import * as card from './@dojo/widgets/card/card.m.css';
+import * as checkbox from './@dojo/widgets/checkbox/checkbox.m.css';
+import * as combobox from './@dojo/widgets/combobox/combobox.m.css';
+import * as dialog from './@dojo/widgets/dialog/dialog.m.css';
+import * as gridBody from './@dojo/widgets/grid-body/grid-body.m.css';
+import * as gridCell from './@dojo/widgets/grid-cell/grid-cell.m.css';
+import * as gridFooter from './@dojo/widgets/grid-footer/grid-footer.m.css';
+import * as gridHeader from './@dojo/widgets/grid-header/grid-header.m.css';
+import * as gridPaginatedFooter from './@dojo/widgets/grid-paginated-footer/grid-paginated-footer.m.css';
+import * as gridPlaceholderRow from './@dojo/widgets/grid-placeholder-row/grid-placeholder-row.m.css';
+import * as gridRow from './@dojo/widgets/grid-row/grid-row.m.css';
+import * as grid from './@dojo/widgets/grid/grid.m.css';
+import * as helperText from './@dojo/widgets/helper-text/helper-text.m.css';
+import * as icon from './@dojo/widgets/icon/icon.m.css';
+import * as label from './@dojo/widgets/label/label.m.css';
+import * as listbox from './@dojo/widgets/listbox/listbox.m.css';
+import * as outlinedButton from './@dojo/widgets/outlined-button/outlined-button.m.css';
+import * as progress from './@dojo/widgets/progress/progress.m.css';
+import * as radio from './@dojo/widgets/radio/radio.m.css';
+import * as raisedButton from './@dojo/widgets/raised-button/raised-button.m.css';
+import * as rangeSlider from './@dojo/widgets/range-slider/range-slider.m.css';
+import * as select from './@dojo/widgets/select/select.m.css';
+import * as slidePane from './@dojo/widgets/slide-pane/slide-pane.m.css';
+import * as slider from './@dojo/widgets/slider/slider.m.css';
+import * as snackbar from './@dojo/widgets/snackbar/snackbar.m.css';
+import * as splitPane from './@dojo/widgets/split-pane/split-pane.m.css';
+import * as tabController from './@dojo/widgets/tab-controller/tab-controller.m.css';
+import * as textArea from './@dojo/widgets/text-area/text-area.m.css';
+import * as textInput from './@dojo/widgets/text-input/text-input.m.css';
+import * as timePicker from './@dojo/widgets/time-picker/time-picker.m.css';
+import * as titlePane from './@dojo/widgets/title-pane/title-pane.m.css';
+import * as toolbar from './@dojo/widgets/toolbar/toolbar.m.css';
+import * as tooltip from './@dojo/widgets/tooltip/tooltip.m.css';
+
+export default {
+	'@dojo/widgets/accordion-pane': accordionPane,
+	'@dojo/widgets/button': button,
+	'@dojo/widgets/calendar': calendar,
+	'@dojo/widgets/card': card,
+	'@dojo/widgets/checkbox': checkbox,
+	'@dojo/widgets/combobox': combobox,
+	'@dojo/widgets/dialog': dialog,
+	'@dojo/widgets/grid-body': gridBody,
+	'@dojo/widgets/grid-cell': gridCell,
+	'@dojo/widgets/grid-footer': gridFooter,
+	'@dojo/widgets/grid-header': gridHeader,
+	'@dojo/widgets/grid-paginated-footer': gridPaginatedFooter,
+	'@dojo/widgets/grid-placeholder-row': gridPlaceholderRow,
+	'@dojo/widgets/grid-row': gridRow,
+	'@dojo/widgets/grid': grid,
+	'@dojo/widgets/helper-text': helperText,
+	'@dojo/widgets/icon': icon,
+	'@dojo/widgets/label': label,
+	'@dojo/widgets/listbox': listbox,
+	'@dojo/widgets/outlined-button': outlinedButton,
+	'@dojo/widgets/progress': progress,
+	'@dojo/widgets/radio': radio,
+	'@dojo/widgets/raised-button': raisedButton,
+	'@dojo/widgets/range-slider': rangeSlider,
+	'@dojo/widgets/select': select,
+	'@dojo/widgets/slide-pane': slidePane,
+	'@dojo/widgets/slider': slider,
+	'@dojo/widgets/snackbar': snackbar,
+	'@dojo/widgets/split-pane': splitPane,
+	'@dojo/widgets/tab-controller': tabController,
+	'@dojo/widgets/text-area': textArea,
+	'@dojo/widgets/text-input': textInput,
+	'@dojo/widgets/time-picker': timePicker,
+	'@dojo/widgets/title-pane': titlePane,
+	'@dojo/widgets/toolbar': toolbar,
+	'@dojo/widgets/tooltip': tooltip
+};
